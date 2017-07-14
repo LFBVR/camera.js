874ad00f9152789c6bb162d02a3653f538127119
mlefebvre <maxence.lfbvr@gmail.com>
2017-07-14 13:27:58 +0200

    allow manual tick mode

--- camera.js
+++ camera.js
@@ -6,6 +6,10 @@ import DEFAULT_OPTIONS from "./defaults";
 export default class Camera {
     constructor(captureOptions) {
         this.options = merge({}, DEFAULT_OPTIONS, captureOptions);
+
+        if (!captureOptions.container) {
+            throw Error('You should specify a container for your camera');
+        }
     }
 
     async init() {
@@ -32,7 +36,8 @@ export default class Camera {
 
                     if (video.mozSrcObject !== undefined) { // hack for Firefox < 19
                         video.mozSrcObject = stream;
-                    } else {
+                    }
+                    else {
                         const URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
                         video.src = (URL && URL.createObjectURL(stream)) || stream;
                     }
@@ -62,42 +67,56 @@ export default class Camera {
     }
 
     start() {
-        const {video, context, canvas} = this;
+        const {video} = this;
         const {onFrame, fps} = this.options;
         video.play();
 
+        if (onFrame) { // only draw if callback specified
+            return;
+        }
+
         this.renderTimer = setInterval(
-            () => {
-                try {
-                    context.drawImage(video, 0, 0, video.width, video.height);
-                    onFrame(canvas);
-                } catch (e) {
-                    // TODO
-                }
-            },
+            this.tick(onFrame),
             Math.round(1000 / fps)
         );
     }
 
+    tick(onFrame) {
+        return () => {
+            try {
+                onFrame(this.getFrame());
+            } catch (e) {
+                // TODO
+            }
+        };
+    }
+
+    getFrame() {
+        const {video, context, canvas} = this;
+        context.drawImage(video, 0, 0, video.width, video.height);
+        return canvas;
+    }
+
     pause() {
         const {renderTimer, video} = this;
+
         if (renderTimer) {
             clearInterval(renderTimer);
         }
         video.pause();
+
     }
 
     stop() {
         const {video, stream} = this;
-        
         this.pause();
 
+        // STOP CAMERA NOT JUST FEED
+        stream.getTracks().forEach(track => track.stop());
+
         if (video.mozSrcObject !== undefined) {
-            video.mozSrcObject = null;
-        } else {
-            video.src = "";
-            // STOP CAMERA NOT JUST FEED
-            stream.getTracks().forEach(track => track.stop());
+            return video.mozSrcObject = null;
         }
+        video.src = "";
     }
 }
--- defaults.js
+++ defaults.js
@@ -1,18 +1,11 @@
 'use strict';
 
-const noop = () => {
-};
-
 const DEFAULT_OPTIONS = {
     fps: 30,
     width: 640,
     height: 480,
     mirror: false,
-    targetCanvas: null,
-    onSuccess: noop,
-    onError: noop,
-    onNotSupported: noop,
-    onFrame: noop
+    container: null
 };
 
 export default DEFAULT_OPTIONS;
